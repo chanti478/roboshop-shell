699fde06e4f72445e97598c3d5c712029dc96cc5
chanti478 <info@joindevops.com>
2023-08-22 20:08:25 +0530

    created  mongoDB

---
+++ mongoDB.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript1-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1
+fi
+VALIDATE()
+{
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R failure $N"
+        exit 1
+    else
+        echo -e "$2 ... $G success $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied mongoDB repo into yum.repos.d"
+
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "Enabling MongoDB"
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "sTARTING MONGODB"
+
+se -i 's/127.0.0.1/0.0.0.0//etc/mongod.conf' &>> $LOGFILE
+
+VALIDATE $? "Edited MongoDB conf"
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "RESTARTING mongoDB"
