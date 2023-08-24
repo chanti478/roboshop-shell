1c2d307fa4d7ddc8c826d3626349399009d552e6
chanti478 <info@joindevops.com>
2023-08-24 15:24:05 +0530

    added catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -27,71 +27,71 @@ VALIDATE()
     fi
 }
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>>$LOGFILE
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 
 VALIDATE $? "setting up NPM source"
 
-yum install nodejs -y &>>>$LOGFILE
+yum install nodejs -y &>>$LOGFILE
 
 VALIDATE $? "Installing nodejs"
 
 # once the user is created, if you run this script 2nd time
 #IMPROVEMENT: first check the user already exist or not, if not exist and then create
 
-useradd roboshop &>>>$LOGFILE
+useradd roboshop &>>$LOGFILE
 
 # write a condition to check directory already exist or not
-mkdir /app &>>>$LOGFILE
+mkdir /app &>>$LOGFILE
 
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>>$LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "dowloading catalogue artifact"
 
-cd /app &>>>$LOGFILE
+cd /app &>>$LOGFILE
 
 VALIDATE $? "moving in to app directory"
 
-unzip /tmp/catalogue.zip &>>>$LOGFILE
+unzip /tmp/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "unzipping catalogue folder"
 
-cd /app &>>>$LOGFILE
+cd /app &>>$LOGFILE
 
 VALIDATE $? "moving back to app dir"
 
-npm install &>>>$LOGFILE
+npm install &>>$LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalog.service because we are inside /app dir
 
-cp /home/devops/daws-74s/repos/roboshop-shell/catalog.service /etc/systemd/system/catalogue.service &>>>$LOGFILE
+cp /home/devops/daws-74s/repos/roboshop-shell/catalog.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
-systemctl daemon-reload &>>>$LOGFILE
+systemctl daemon-reload &>>$LOGFILE
 
 VALIDATE $? "daemon reload"
 
-systemctl enable catalogue &>>>$LOGFILE
+systemctl enable catalogue &>>$LOGFILE
 
 VALIDATE $? "enable catalogue"
 
 
-systemctl start catalogue &>>>$LOGFILE
+systemctl start catalogue &>>$LOGFILE
 
 VALIDATE $? "start catalogue"
 
 
-cp /home/devops/daws-74s/repos/roboshop-shell//etc/yum.repos.d/mongo.repo &>>>$LOGFILE
+cp /home/devops/daws-74s/repos/roboshop-shell//etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "copying Mongo repo"
 
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>$LOGFILE
 
 VALIDATE $? "installing mongo client"
 
-mongo --host mongoDB.joindevops.online </app/schema/catalogue.js
+mongo --host mongoDB.joindevops.online </app/schema/catalogue.js &>>$LOGFILE
 
 VALIDATE $? "loading catalogue data in to mongodb"
