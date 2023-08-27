389920bc175a0adf60a309af7d75c2cdcc6f8d3e
chanti478 <info@joindevops.com>
2023-08-27 09:50:31 +0530

    added serviers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0486e34cb496ac651
+DOMAIN_NAME=joindevops.online
+
+# if mysql or mongodb instance_type should be t3.medium, but all others is t2,micro
 
 for i in "${NAMES[@]}"
 do
-    echo "NAME: $i"
+    if [[$i == "mongodb" || $i =="mysql"]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID
+     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 
+    aws route53 change-resource-record-sets --hosted-zone-id Z0856705XOSJKVEOO20B --change-batch
+
+{
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "$i.$DOMAIN_NAME",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
+}}]
+}
+'
 done
 
