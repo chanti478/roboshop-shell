632b42ef9487d67b7e4f92376c4a122495b8d3ea
chanti478 <info@joindevops.com>
2023-08-27 10:39:51 +0530

    added serviers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,7 +10,7 @@ SECURITY_GROUP_ID=sg-0486e34cb496ac651
 
 for i in "${NAMES[@]}"
 do
-    if [[ $i == "mongodb" || $i =="mysql" ]]
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
