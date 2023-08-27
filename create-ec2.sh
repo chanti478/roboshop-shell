8e12deb4629285fe88e03f97ed975189728aeac3
chanti478 <info@joindevops.com>
2023-08-27 10:36:23 +0530

    added serviers script

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,7 +10,7 @@ DOMAIN_NAME=joindevops.online
 
 for i in "${NAMES[@]}"
 do
-    if [[$i == "mongodb" || $i =="mysql"]]
+    if [[ $i == "mongodb" || $i =="mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
@@ -22,19 +22,19 @@ do
     $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z0856705XOSJKVEOO20B --change-batch '
+   # aws route53 change-resource-record-sets --hosted-zone-id Z0856705XOSJKVEOO20B --change-batch '
 
-    {
+  #  {
 
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                                    "Name": "'$i.$DOMAIN_NAME'",
-                                    "Type": "A",
-                                    "TTL": 300,
-                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                                }}]
-    }
-    '
+   #         "Changes": [{
+   #         "Action": "CREATE",
+   #                     "ResourceRecordSet": {
+   #                                 "Name": "'$i.$DOMAIN_NAME'",
+   #                                 "Type": "A",
+   #                                 "TTL": 300,
+   #                              "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+   #                             }}]
+  #  }
+ #   '
 done
 
